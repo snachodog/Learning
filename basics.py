af8764b2ab48ad4fc3788a9d083616aab4fd88d8
Steve  Dogiakos <dogiakos@gmail.com>
2020-05-17 19:32:28 -0600

    First commit
    Eps. 2-5

---
+++ basics.py
@@ -0,0 +1,20 @@
+if True:
+    print 'hello'
+    print 'asd'
+
+# single-line comment
+
+"""
+line 1
+line 2
+line 3
+"""
+
+name = 'snachodog'
+num1 = 5; num2 = 8
+
+long_name =\
+    "something" + \
+    "something else"
+
+print 'hello world',
